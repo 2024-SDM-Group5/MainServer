77debac3c5b3c036e567e59ff257ee8f63fe9c29
ntu-ben <pohan.ho@gmail.com>
2024-04-10 14:20:59 +0800

    [dev] add bot.py route

--- app/main.py
+++ app/main.py
@@ -39,7 +39,7 @@ app.include_router(maps.router)
 app.include_router(restaurants.router)
 app.include_router(comments.router)
 app.include_router(diaries.router)
-# app.include_router(bots.router)
+app.include_router(bots.router)
 
 # Optional: Simple root endpoint to check API status
 @app.get("/") 
